66b2304d578a9619bf8bfb9515b8c7898e924e29
Matthew Else <matthewelse1997@gmail.com>
2025-07-11 08:42:37 -0400

    Remove `test_call`, tweak runtime

--- example/test_call/main.ml
+++
@@ -1,7 +0,0 @@
-external print_int : i32 -> i32 = "mucaml_print"
-
-let main x =
-  let y = x + 5 in
-  let z = print_int y in
-  z + x
-
--- runtime/src/runtime.zig
+++ runtime/src/runtime.zig
@@ -4,7 +4,7 @@ const std = @import("std");
 // Core runtime functions that are available on all targets
 extern fn mucaml_main(i32) u32;
 
-export fn string_length(s: [*]const u8) usize {
+fn strlen(s: [*]const u8) usize {
     var length: usize = 0;
     var ptr = s;
 
@@ -16,7 +16,6 @@ export fn string_length(s: [*]const u8) usize {
     return length;
 }
 
-// Native target print implementation (only compile for native)
 comptime {
     if (std.mem.eql(u8, config.mu_target, "native")) {
         @export(&mucaml_print_endline, .{ .name = "mucaml_print_endline", .linkage = .strong });
@@ -25,7 +24,7 @@ comptime {
 
 fn mucaml_print_endline(x: [*]const u8) callconv(.C) i32 {
     const out_file = std.io.getStdOut();
-    const len = string_length(x);
+    const len = strlen(x);
     (out_file.writer().print("{s}\n", .{x[0..len]})) catch {};
     return 0;
 }
